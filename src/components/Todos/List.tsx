c8f156fccc6b7c46591172b2a518fcd3e479c374
Vlatko Trajkov <trajkovvlatko@gmail.com>
2020-07-09 13:35:13 +0200

    Separated Todos list

---
+++ src/components/Todos/List.tsx
@@ -0,0 +1,93 @@
+import React, {useState, useEffect} from 'react';
+import firebase from '../../firebase';
+import IContentItem from '../../interfaces/IContentItem';
+import ContentList from '../../components/ContentList/ContentList';
+import ContentGroupedList from '../../components/ContentGroupedList/ContentGroupedList';
+import {group} from '../../helpers/main';
+
+const db = firebase.database();
+const pendingRef = db.ref('todos/pending');
+const completedRef = db.ref('todos/completed');
+
+function List() {
+  const [completed, setCompleted] = useState<IContentItem[]>([]);
+  const [pending, setPending] = useState<IContentItem[]>([]);
+
+  useEffect(() => {
+    const unsubscribePending = pendingRef.on('value', (snapshot) => {
+      const pendingList: IContentItem[] = [];
+      snapshot.forEach((doc) => {
+        pendingList.push({...doc.val(), key: doc.key});
+      });
+      if (pendingList.length !== pending.length) {
+        setPending(pendingList);
+      }
+    });
+
+    const unsubscribeCompleted = completedRef
+      .orderByKey()
+      .limitToLast(5)
+      .on('value', (snapshot) => {
+        const completedList: IContentItem[] = [];
+        snapshot.forEach((doc) => {
+          completedList.push({...doc.val(), key: doc.key});
+        });
+        const oldKeys = JSON.stringify(completed.map((m) => m.key).sort());
+        const newKeys = JSON.stringify(completedList.map((m) => m.key).sort());
+        if (oldKeys !== newKeys) {
+          setCompleted(completedList.reverse());
+        }
+      });
+
+    return () => {
+      pendingRef.off('value', unsubscribePending);
+      completedRef.off('value', unsubscribeCompleted);
+    };
+  });
+
+  const toggle = async (bucket: string, key: string) => {
+    try {
+      let from: firebase.database.Reference;
+      let to: firebase.database.Reference;
+      if (bucket === 'pending') {
+        from = pendingRef;
+        to = completedRef;
+      } else if (bucket === 'completed') {
+        from = completedRef;
+        to = pendingRef;
+      } else {
+        console.error('Invalid bucket name.');
+        return;
+      }
+      const snap = await from.child(key).once('value');
+      const val = {...snap.val()};
+      to.push().set(val);
+      from.child(key).remove();
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <div className='todos-lists'>
+      <div>
+        <b>Pending</b>
+      </div>
+      <br />
+      <div className='pending-list'>
+        <ContentGroupedList
+          list={group(pending)}
+          toggle={{cb: toggle, bucket: 'pending'}}
+        />
+      </div>
+
+      <b>Completed</b>
+      <ContentList
+        list={completed}
+        toggle={{cb: toggle, bucket: 'completed'}}
+      />
+    </div>
+  );
+}
+
+export default List;
--- src/pages/Todos/Todos.tsx
+++ src/pages/Todos/Todos.tsx
@@ -1,56 +1,15 @@
-import React, {useContext, useRef, useState, useEffect} from 'react';
+import React, {useContext, useRef} from 'react';
 import firebase from '../../firebase';
 import UserContext from '../../contexts/UserContext';
-import IContentItem from '../../interfaces/IContentItem';
-import {group} from '../../helpers/main';
 
 import './style.css';
-import ContentList from '../../components/ContentList/ContentList';
-import ContentGroupedList from '../../components/ContentGroupedList/ContentGroupedList';
-
-const db = firebase.database();
-const pendingRef = db.ref('todos/pending');
-const completedRef = db.ref('todos/completed');
+import List from '../../components/Todos/List';
 
 function Todos() {
   const {user} = useContext(UserContext);
   const todoEl = useRef<HTMLInputElement>(null);
   const dueDateEl = useRef<HTMLInputElement>(null);
   const durationEl = useRef<HTMLInputElement>(null);
-  const [completed, setCompleted] = useState<IContentItem[]>([]);
-  const [pending, setPending] = useState<IContentItem[]>([]);
-
-  useEffect(() => {
-    const unsubscribePending = pendingRef.on('value', (snapshot) => {
-      const pendingList: IContentItem[] = [];
-      snapshot.forEach((doc) => {
-        pendingList.push({...doc.val(), key: doc.key});
-      });
-      if (pendingList.length !== pending.length) {
-        setPending(pendingList);
-      }
-    });
-
-    const unsubscribeCompleted = completedRef
-      .orderByKey()
-      .limitToLast(5)
-      .on('value', (snapshot) => {
-        const completedList: IContentItem[] = [];
-        snapshot.forEach((doc) => {
-          completedList.push({...doc.val(), key: doc.key});
-        });
-        const oldKeys = JSON.stringify(completed.map((m) => m.key).sort());
-        const newKeys = JSON.stringify(completedList.map((m) => m.key).sort());
-        if (oldKeys !== newKeys) {
-          setCompleted(completedList.reverse());
-        }
-      });
-
-    return () => {
-      pendingRef.off('value', unsubscribePending);
-      completedRef.off('value', unsubscribeCompleted);
-    };
-  });
 
   const save = () => {
     if (
@@ -71,7 +30,11 @@ function Todos() {
       ? parseInt(durationEl.current.value)
       : '';
     const dueDate = dueDateEl.current.value;
-    pendingRef.push().set({user, note, duration, dueDate, at: Date()});
+    firebase
+      .database()
+      .ref('todos/pending')
+      .push()
+      .set({user, note, duration, dueDate, at: Date()});
     todoEl.current.value = '';
     durationEl.current.value = '';
     dueDateEl.current.value = '';
@@ -83,51 +46,11 @@ function Todos() {
     save();
   };
 
-  const toggle = async (bucket: string, key: string) => {
-    try {
-      let from: firebase.database.Reference;
-      let to: firebase.database.Reference;
-      if (bucket === 'pending') {
-        from = pendingRef;
-        to = completedRef;
-      } else if (bucket === 'completed') {
-        from = completedRef;
-        to = pendingRef;
-      } else {
-        console.error('Invalid bucket name.');
-        return;
-      }
-      const snap = await from.child(key).once('value');
-      const val = {...snap.val()};
-      to.push().set(val);
-      from.child(key).remove();
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-
   return (
     <>
       <h1>Todos</h1>
 
-      <div className='todos-lists'>
-        <div>
-          <b>Pending</b>
-        </div>
-        <br />
-        <div className='pending-list'>
-          <ContentGroupedList
-            list={group(pending)}
-            toggle={{cb: toggle, bucket: 'pending'}}
-          />
-        </div>
-
-        <b>Completed</b>
-        <ContentList
-          list={completed}
-          toggle={{cb: toggle, bucket: 'completed'}}
-        />
-      </div>
+      <List />
 
       <div className='todos-form'>
         <input
